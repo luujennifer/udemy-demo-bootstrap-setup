d6471abe41f56638f0f0f8461fdf5532bb0aa7c0
Jennifer Luu <58239721+luujennifer@users.noreply.github.com>
2020-11-10 10:32:12 +1100

    commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'udemy-demo-bootstrap-setup';
+export class AppComponent { 
 }
