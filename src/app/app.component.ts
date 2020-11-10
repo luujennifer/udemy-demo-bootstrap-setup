263b876ed2001f912fd33b3139aab81bb7aca7ee
Jennifer Luu <58239721+luujennifer@users.noreply.github.com>
2020-11-10 12:38:57 +1100

    COMMIT 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css'] // EXTERNAL FILE
   styles: [`
     h3{
-      color: dodgerblue;
+      color: dodgerblue; 
     }
   `]
 })
